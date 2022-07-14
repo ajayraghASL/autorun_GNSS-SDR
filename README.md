2c879ad1b7bd56ba073c1b8a22bb235b4175df57
ajayraghASL <ajayragh@augsenselab.com>
2022-07-14 14:47:52 +0530

    python script and readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # autorun_GNSS-SDR
 Python script to run gnss-sdr for a given amount of time and save the generated OBS and NAV to the specified directory. 
+
+## Usage
+
+~~~
+python gnss_run.py -r 3600
+~~~
+
+The parameter 'r' passes the duration for which gnss-sdr is to be run. After running gnss-sdr for the given duration the script moves the obs and nav files generated in the current working directory to the dropbox folder in the system.
+
+### Note: 
+Change the dropbox directory path to the one in your pc within the code
---
+++ gnss_run.py
@@ -0,0 +1,46 @@
+import time
+import glob
+import os.path
+import datetime
+import shutil
+import argparse
+from subprocess import Popen, PIPE
+
+# directory paths
+dropbox_dir = "/home/ajay/Dropbox/GPSDataSet/"
+obs = "./*.22O"
+nav = "./*.22N"
+
+# variable for gnss-sdr runtime taking time in seconds provided from terminal
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--runtime', default = 3600, dest = 'runtime', help = 'provide runtime in seconds', type= int)
+args = parser.parse_args()
+run_time = args.runtime
+
+#Python subprocess to run gnss-sdr for the prescribed time witte
+p = Popen(["gnss-sdr --config_file=./rtlsdr.conf"], stdin=PIPE, shell=True)
+time.sleep(run_time)
+p.communicate(input=b'q')
+
+#finding the newly created obs and nav files from provided directory
+obs_files = glob.glob(obs)
+nav_files = glob.glob(nav)
+new_obs = max(obs_files, key=os.path.getctime)
+new_nav = max(nav_files, key=os.path.getctime)
+new_obs_name = os.path.basename(new_obs)
+new_nav_name = os.path.basename(new_nav)
+
+#extracting the created time of the latest file in the directory 
+obs_ctime = os.path.getctime(new_obs)
+new_dir = str(datetime.datetime.fromtimestamp(obs_ctime))
+
+new_dir_path = dropbox_dir+new_dir
+#creating new folder in dropbox
+if (os.path.isdir(new_dir_path)):
+    print("The file directory already exists in dropbox. So the file is not being copied")
+    print("Exiting execution")
+    exit()
+else:
+    os.mkdir(new_dir_path)
+    shutil.move(new_obs,new_dir_path+"/"+new_obs_name)
+    shutil.move(new_nav,new_dir_path+"/"+new_nav_name)
